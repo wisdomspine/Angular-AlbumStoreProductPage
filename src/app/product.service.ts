4221e2be439ad2d9fb481d9b781933427db6d8a1
wisdomspine <sabopriest@gmail.com>
2019-11-08 14:49:16 +0100

    Product-list-component created

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,15 +4,21 @@ import { Http, Response } from '@angular/http';
 import { Album } from './album';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  _productsUrl = '../assets/products.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
    return this._http.get(this._albumUrl).pipe(map((response) => <Album>response.json()));
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).pipe(map(response => <Product[]>response.json()));
   }
 }
