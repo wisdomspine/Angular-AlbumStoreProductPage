374fb5e18c92151582987dc918d5589b7e8dc14c
wisdomspine <sabopriest@gmail.com>
2019-11-08 13:42:01 +0100

    ***

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -8,9 +9,9 @@ import { map } from 'rxjs/operators';
 export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
-
   getAlbum(id: number) {
     // tslint:disable-next-line
-   return this._http.get(this._albumUrl).pipe(map(response => response.json()));
+    
+   return this._http.get(this._albumUrl).pipe(map((response) => response.json()));
   }
 }
